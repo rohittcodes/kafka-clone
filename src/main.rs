65334c1a6fdf432987ca1bdfa7057b1e80929c37
rohittcodes <rohittcodes@gmail.com>
2025-02-26 09:46:30 +0530

    impl: correlation-id

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
-
+use std::{io::Write, net::TcpListener};
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
+                let buf: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 7];
+                _stream.write(&buf).unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
